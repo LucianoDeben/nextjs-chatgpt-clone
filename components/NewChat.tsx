e06ec514ed0f8e2521203415dfdce30a6915ef55
LucidCodeAI <lutchocode@gmail.com>
2023-11-10 16:34:06 +0100

    Add chat UI logic and ID routing

--- components/NewChat.tsx
+++ components/NewChat.tsx
@@ -1,9 +1,29 @@
+"use client";
+
+import { db } from "@/utils/firebase";
 import { PlusIcon } from "@heroicons/react/24/solid";
+import { collection, addDoc, serverTimestamp } from "firebase/firestore";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 type Props = {};
 const NewChat = (props: Props) => {
+  const { data: session } = useSession();
+  const router = useRouter();
+
+  const createNewChat = async () => {
+    const doc = await addDoc(
+      collection(db, "users", session?.user?.email!, "chats"),
+      {
+        userId: [session?.user?.email!],
+        createdAt: serverTimestamp(),
+      }
+    );
+    router.push(`/chats/${doc.id}`);
+  };
+
   return (
-    <div className="border border-gray-700 chatRow">
+    <div className="border border-gray-700 chatRow" onClick={createNewChat}>
       <PlusIcon className="h-4 w-4"></PlusIcon>
       <p> New Chat</p>
     </div>
--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,12 +1,25 @@
 "use client";
 
 import { useSession, signOut } from "next-auth/react";
+import { useCollection } from "react-firebase-hooks/firestore";
+import { collection, orderBy, query } from "firebase/firestore";
 import NewChat from "./NewChat";
 import Image from "next/image";
+import { db } from "@/utils/firebase";
+import ChatRow from "./ChatRow";
 
 type Props = {};
 function SideBar({}: Props) {
   const { data: session } = useSession();
+
+  const [chats, loading, error] = useCollection(
+    session &&
+      query(
+        collection(db, "users", session?.user?.email!, "chats"),
+        orderBy("createdAt", "asc")
+      )
+  );
+
   return (
     <div className="p-2 flex flex-col h-screen">
       <div className=" flex-1">
@@ -14,6 +27,9 @@ function SideBar({}: Props) {
           <NewChat></NewChat>
           <div>{/* Models */}</div>
           {/* Map of Chats */}
+          {chats?.docs.map((chat) => (
+            <ChatRow key={chat.id} id={chat.id} />
+          ))}
         </div>
       </div>
       {session && (
