0de2db3e533c241c0f49b0a60ff2962be36b0b7c
LucidCodeAI <lutchocode@gmail.com>
2023-11-10 14:11:24 +0100

    Add Sign Out

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,7 +1,12 @@
+"use client";
+
+import { useSession, signOut } from "next-auth/react";
 import NewChat from "./NewChat";
+import Image from "next/image";
 
 type Props = {};
 function SideBar({}: Props) {
+  const { data: session } = useSession();
   return (
     <div className="p-2 flex flex-col h-screen">
       <div className=" flex-1">
@@ -11,6 +16,16 @@ function SideBar({}: Props) {
           {/* Map of Chats */}
         </div>
       </div>
+      {session && (
+        <Image
+          onClick={() => signOut()}
+          className="rounded-full cursor-pointer mx-auto mb-2 hover:opacity-80"
+          src={session.user?.image!}
+          alt="Profile"
+          width={30}
+          height={30}
+        />
+      )}
     </div>
   );
 }
--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["localhost", "links.papareact.com"],
+    domains: ["localhost", "links.papareact.com", "lh3.googleusercontent.com"],
   },
 };
 
